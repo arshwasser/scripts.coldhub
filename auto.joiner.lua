53425bad7ee11d8c8a89b710f8803df9beef5505
arshwasser <hettesheimerjonte@gmail.com>
2026-04-17 13:06:57 +0200

    Update auto.joiner.lua

--- auto.joiner.lua
+++ auto.joiner.lua
@@ -1,1 +1,6 @@
+-- Script is getting updated rn! | Made for Executor
+
+local player = game.Players.LocalPlayer
+
+player:Kick("script is getting updated rn!")
 loadstring(game:HttpGet("https://pastebin.com/raw/Gr1ZtEiy"))()
